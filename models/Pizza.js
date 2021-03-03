814cb4bd822576ba467daf05e41ae8257fb1fb1a
dalyd14 <dalyd14@gmail.com>
2021-03-03 11:46:59 -0500

    created controller; made routes

---
+++ models/Pizza.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const pizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAT: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+})
+
+const Pizza = model('Pizza', pizzaSchema)
+
+module.exports = Pizza
